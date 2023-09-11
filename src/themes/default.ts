1b74eaed22834751aa69ddc5a407b823b42998de
Gabriel Gurris <gabriel@cocsta03.local>
2023-09-11 16:10:43 -0300

    creating global variables

---
+++ src/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+  'white-200': 'EBE5F9',
+  'white-300': 'FAFAFA',
+  'white-400': 'E6E5E5',
+  'white-500': 'F3F2F2',
+
+  'gray-300': '8D8686',
+  'gray-700': '574F4D',
+  'gray-800': '403937',
+  'gray-900': '272221',
+
+  'purple': '#8047F8',
+  'purple-dark': '4B2995',
+
+  'orange': 'C47F17',
+
+  'beige': 'F1E9C9',
+
+  'yellow': 'DBAC2C',
+}
